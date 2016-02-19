e5f24f9ecb3352b9d302d54814f5f739ad4e6161
Niamh Lawlor <nileahtobhair@gmail.com>
2016-02-19 20:36:49 +0000

    fixing display bug

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('F1FeederApp.controllers', ['ngTouch']).
+angular.module('F1FeederApp.controllers', []).
 controller('driversController', function($scope) {
     $scope.employeesList = [
       {
